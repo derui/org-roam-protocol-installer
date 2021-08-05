8bb41ee5fe8530f4255bb057671bb84c1af61f82
derui <derutakayu@gmail.com>
2021-08-05 21:34:30 +0900

    fix: do valid things in applescript

--- src/macos_installer.rs
+++ src/macos_installer.rs
@@ -54,9 +54,10 @@ pub fn new(config: MacOSConfig) -> Box<dyn RoamProtocolInstaller> {
 fn make_application_making_script(path: &Path) -> String {
     let path = path.to_str().unwrap();
     let script = format!(
-        r#"tell application "Script Editor"
-  set file to open {}
-  tell file to save as "application" in "/Application/OrgProtocolClient.app"
+        r#"
+tell application "Script Editor"
+  open "{}"
+  save as "application" in "/Application/OrgProtocolClient.app"
   quit
 end tell
 "#,
